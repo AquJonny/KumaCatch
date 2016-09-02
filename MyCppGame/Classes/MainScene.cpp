1bbfd76f5dd9b2c97f88a1dd982a8bcfe7469018
AquJonny@Mac <aquarius.joker@gmail.com>
2016-09-02 12:02:10 +0900

    base source

---
+++ MyCppGame/Classes/MainScene.cpp
@@ -0,0 +1,9 @@
+//
+//  MainScene.cpp
+//  MyCppGame
+//
+//  Created by Xuhao on 16/9/2.
+//
+//
+
+#include "MainScene.h"
---
+++ MyCppGame/Classes/MainScene.h
@@ -0,0 +1,14 @@
+//
+//  MainScene.h
+//  MyCppGame
+//
+//  Created by Xuhao on 16/9/2.
+//
+//
+
+#ifndef MainScene_h
+#define MainScene_h
+
+#include <stdio.h>
+
+#endif /* MainScene_h */
